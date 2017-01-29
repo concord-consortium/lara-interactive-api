2beed9f0b896f61d7a6f485bed9e15d6bbd4fc9c
Doug Martin <doug@zoopdoop.com>
2017-01-29 16:39:50 -0500

    Adds a simple interactive that uses the getAuthInfo command [#138281165]

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,17 +2,18 @@ var gulp = require('gulp');
 var config = require('../config');
 
 gulp.task('watch', function() {
-    gulp.watch(config.css.watch,                ['css']);
-    gulp.watch(config.coffeelint.watch,         ['coffeelint']);
-    gulp.watch(config.browserify.app.watch,     ['browserify-app']);
-    gulp.watch(config.browserify.iframe.watch,  ['browserify-iframe']);
-    gulp.watch(config.browserify.saver.watch,   ['browserify-saver']);
-    gulp.watch(config.browserify.globals.watch, ['browserify-globals']);
-    gulp.watch(config.assets.watch,             ['assets']);
-    gulp.watch(config.vendor.watch,             ['vendor']);
+    gulp.watch(config.css.watch,                 ['css']);
+    gulp.watch(config.coffeelint.watch,          ['coffeelint']);
+    gulp.watch(config.browserify.app.watch,      ['browserify-app']);
+    gulp.watch(config.browserify.iframe.watch,   ['browserify-iframe']);
+    gulp.watch(config.browserify.saver.watch,    ['browserify-saver']);
+    gulp.watch(config.browserify.globals.watch,  ['browserify-globals']);
+    gulp.watch(config.browserify.userInfo.watch, ['browserify-user-info']);
+    gulp.watch(config.assets.watch,              ['assets']);
+    gulp.watch(config.vendor.watch,              ['vendor']);
 });
 
 gulp.task('build-all', ['coffeelint', 'browserify-app', 'browserify-globals',
-  'browserify-iframe', 'browserify-saver', 'css', 'assets', 'vendor']);
+  'browserify-iframe', 'browserify-saver', 'browserify-user-info', 'css', 'assets', 'vendor']);
 
 gulp.task('default', ['build-all', 'watch']);
